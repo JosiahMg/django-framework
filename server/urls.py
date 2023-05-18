ed67b7d6dbed3996f67c3a3ce86e987ac72a9658
menghui <menghui3040@163.com>
2023-05-18 14:30:38 +0800

    url

--- server/urls.py
+++ server/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('trace/', include('api.urls'))
+    path('django-framework/', include('api.urls'))
 ]
